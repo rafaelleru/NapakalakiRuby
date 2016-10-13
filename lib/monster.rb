d6f7426ae1a47c53e9bacecc715b8a45deb593e8
archdri <archdri@archdri>
2016-10-13 16:15:06 +0200

    1ª version de la clase Monster

--- lib/monster.rb
+++ lib/monster.rb
@@ -2,4 +2,22 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-puts "Hello World"
+class Monster
+  attr_reader name
+  attr_reader combatLevel
+  attr_reader bc
+  attr_reader prize
+  
+  def initialize(name, combatLevel, bc, prize)
+    @name = name
+    @combatLevel = combatLevel
+    @bc = bc
+    @prize = prize
+  end
+  
+  def to_s
+    "Nombre: #{@name} \n Nivel de combate: #{@combatLevel} \n Mal rollo: #{@bc.to_s}"
+    + " \n Buen rollo: #{@prize.to_s}"
+  end
+  
+end
