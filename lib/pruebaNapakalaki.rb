5b679644239ae439bf5944fe6611bd89e5798b6f
archdri <archdri@archdri>
2016-10-19 12:32:23 +0200

    Añadido require relatives y probando prubaNapakalaki

--- lib/pruebaNapakalaki.rb
+++ lib/pruebaNapakalaki.rb
@@ -2,12 +2,16 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 require_relative 'bad_consequence.rb'
+require_relative "monster.rb"
+require_relative "prize.rb"
 
 class PruebaNapakalaki
   attr_reader :bc
   def saluda()
+    @monster = Monster.new("eaf",14,nil,nil)
     @bc = BadConsequence.new
-    puts @bc.to_s
+    #puts @bc.to_s
+    puts @monster.to_s
   end
 puts "hola"
 p = PruebaNapakalaki.new
