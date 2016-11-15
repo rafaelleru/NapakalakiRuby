7a59d13210b4646d23713490f94c23050264eca8
rafa <rafaelleru95103@gmail.com>
2016-11-15 16:25:53 +0100

    dice done

---
+++ lib/dice.rb
@@ -0,0 +1,13 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+require "singleton"
+class Dice
+  
+  import Singleton
+  
+  def next_number()
+    return 1 + rand(6)
+  end
+end
