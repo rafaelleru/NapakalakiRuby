ae55666adaf7b89be4baf337fbac9e2365538fb2
archdri <archdri@archdri>
2016-12-13 12:18:19 +0100

    p3 napakalaki rub

--- lib/Napakalaki.rb
+++ lib/Napakalaki.rb
@@ -6,19 +6,50 @@ require "singleton"
 class Napakalaki
   include Singleton
 
-  attr_reader :currentPlayer
-  attr_reader :players
-  attr_reader :dealer
-  attr_reader :current_monster
+  attr_accesor :currentPlayer
+  attr_accesor :players
+  attr_accesor :dealer
+  attr_accesor :current_monster
   
   def initPlayers(names)
+    @dealer = CardDealer.instance
+    @player = Array.new
+    
+    names.each do |s|
+      players << Player.new(s)
+    end
   end
   
   def nextPlayer()
     
+    total_players = @players.length
+    
+    if(@currentPlayer == nil) then
+      next_index = rand(total_players)
+    else
+      current_player_index = @players.index(@currentPlayer)
+      if current_player_index == total_players then
+        next_index = 0
+      else
+        next_index = current_player_index + 1 
+      end
+    end
+    
+    next_player = @players.at(next_index)
+    @currentPlayer = next_player
+    return @currentPlayer
+    
   end
   
   def nextTurnIsAllowed()
+    
+    if @currentPlayer == nil then
+      allowed = true
+    else
+      allowed = @currentPlayer.validState
+    end
+    
+    return allowed
     
   end
   
@@ -46,18 +77,23 @@ class Napakalaki
   end
   
   def getCurrentPlayer()
-    
+    return @current_player
   end
   
   def getCurrentMonster()
-    
+    return @current_monster
   end
   
   def nextTurn()
     
   end
   
-  def endOfGame()
-    
+  def endOfGame(result)
+    if result == WINGAME
+      return true
+    else
+      return false
+    end
   end
+  
 end
